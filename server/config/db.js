c1f43a9acb63ebd82422ca99bf581e94cf19b7cd
BradProject <sammykibet300@gmail.com>
2025-10-28 15:51:27 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import { logger } from './logger.js'
 
 export async function connectDB(){
-  const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/dhms'
+  const url = process.env.MONGO_URL || 'mongodb+srv://sammykibet300_db_user:uNn0snFgv8GnuybO@cluster0.8jdyirr.mongodb.net/'
   await mongoose.connect(url)
   logger.info('MongoDB connected')
 }
